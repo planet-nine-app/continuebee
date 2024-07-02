f4196229c18118cfefad6371b5dbc8c407a1889a
zach-planet-nine <zach@planetnine.app>
2024-07-01 23:46:15 -0700

    not quite working

--- test/mocha/test.js
+++ test/mocha/test.js
@@ -28,10 +28,10 @@ let keys = {};
 it('should register a user', async () => {
   keys = await sessionless.generateKeys(() => { return keys; }, () => {return keys;});
 console.log(keys);
-  keys = {
+/*  keys = {
     privateKey: 'd6bfebeafa60e27114a40059a4fe82b3e7a1ddb3806cd5102691c3985d7fa591',
     pubKey: '03f60b3bf11552f5a0c7d6b52fcc415973d30b52ab1d74845f1b34ae8568a47b5f'
-  };
+  };*/
   const payload = {
     timestamp: new Date().getTime() + '',
     pubKey: keys.pubKey,
@@ -57,17 +57,18 @@ it('should check hash', async () => {
   const signature = await sessionless.sign(timestamp + uuid + hash);
 
   const res = await get(`${baseURL}user/${uuid}?timestamp=${timestamp}&hash=${hash}&signature=${signature}`);
-  res.body.userUUID.length.should.equal(36);
+console.log(res.status);
+  res.status.should.equal(202);
 });
 
 it('should save hash', async () => {
   const timestamp = new Date().getTime() + '';
-  const uuid = savedUser.uuid;
+  const userUUID = savedUser.userUUID;
   const hash = 'firstHhash';
   const newHash = 'secondHash';
 
-  const signature = await sessionless.sign(timestamp + uuid + hash + newHash);
+  const signature = await sessionless.sign(timestamp + userUUID + hash + newHash);
 
-  const res = await put(`${baseURL}user/${uuid}/update-hash`, {timestamp, uuid, hash, newHash, signature});
+  const res = await put(`${baseURL}user/update-hash`, {timestamp, userUUID, hash, newHash, signature});
   res.body.userUUID.length.should.equal(36);
 });
