d94e3ced46b1dfdcca591d65c912b0cca2c53008
Daniel-J-Mason <daniel.j.msn@gmail.com>
2024-07-01 19:36:43 -0700

    Convert userUUID from string to UUID

--- java/spring-server/src/main/java/app/planentnine/springcontinuebee/adapter/web/dto/RestUserDto.java
+++ java/spring-server/src/main/java/app/planentnine/springcontinuebee/adapter/web/dto/RestUserDto.java
@@ -3,7 +3,9 @@ package app.planentnine.springcontinuebee.adapter.web.dto;
 import lombok.Builder;
 import lombok.extern.jackson.Jacksonized;
 
+import java.util.UUID;
+
 @Builder
 @Jacksonized
-public record RestUserDto(String userUUID, String pubKey, String hash) {
+public record RestUserDto(UUID userUUID, String pubKey, String hash) {
 }
