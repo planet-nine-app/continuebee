dd4054959cfe840dadfa77c9f8e03ab2d13dff2a
Daniel-J-Mason <daniel.j.msn@gmail.com>
2024-07-01 19:36:23 -0700

    fix casing

--- java/spring-server/src/main/java/app/planentnine/springcontinuebee/application/validation/CreateUserValidator.java
+++ java/spring-server/src/main/java/app/planentnine/springcontinuebee/application/validation/CreateUserValidator.java
@@ -25,7 +25,7 @@ public class CreateUserValidator {
                     ecNamedCurveParameterSpec.getCurve().decodePoint(publicKeyFormatted.toByteArray());
             
             if (publicKeyPoint == null || !publicKeyPoint.isValid()) {
-                errors.add("Invalid Key format");
+                errors.add("Invalid key format");
             }
         } catch (Exception e) {
             errors.add("Invalid Key format");
